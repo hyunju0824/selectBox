c89bcdc2593c69bea2d4b9617ea60116f0082b20
정현주 <hyunjuhj0824@gmail.com>
2024-01-19 00:47:03 +0900

    첫번째 select box

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -2,7 +2,11 @@ import { selectList } from "../recoil/selectors/selector"
 import { useRecoilValue } from "recoil"
 
 export default function SelectBox() {
-    const selectData = useRecoilValue(selectList)
+    const selectData = useRecoilValue(selectList);
+    console.log(selectData.type);
+    console.log(selectData.type.default);
+
+    console.log(selectData.type.options.food);
     return (
         <div className="w-96 h-96 m-auto mt-32 flex flex-col rounded-lg items-center bg-cyan-50">
             <div className="w-52 m-auto">
@@ -10,18 +14,22 @@ export default function SelectBox() {
                     My Select Box
                 </label>
                 <select
-                    id="location"
-                    name="location"
+                    id="type"
+                    name="type"
                     className="mt-5 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue="Canada"
+                    defaultValue= {selectData.type.default}
                 >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
+                    {
+                        Object.keys(selectData.type.options).map((typeName) => (
+                            <option value={typeName} key={typeName}>
+                                {typeName}
+                            </option>
+                        ))    
+                    }
                 </select>
                 <select
-                    id="location"
-                    name="location"
+                    id="type"
+                    name="type"
                     className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     defaultValue="Canada"
                 >
