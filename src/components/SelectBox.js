87547f8ec899b6d447de5cbc866bb0e4d38a3268
hyunju0824 <hyunjuhj0824@gmail.com>
2024-01-19 02:25:38 +0900

    두번째 select box defalut 값 안들어오던 것 수정

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -39,8 +39,8 @@ export default function SelectBox() {
                     id="type"
                     name="type"
                     className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue={selectData.type.options[selectedType].default}
-                >
+                    value={selectData.type.options[selectedType].default}
+                    >
                     {
                         selectData.type.options[selectedType].options.map((optionName) => (
                             <option value={optionName} key={optionName}>
