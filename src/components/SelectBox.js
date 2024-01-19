6be131cf545dbd5fb1b0cd8feb8dda2aa891838c
hyunju0824 <hyunjuhj0824@gmail.com>
2024-01-20 00:46:11 +0900

    이상하게 작동되는 것을 고쳐야 한다 ㅠ

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { selectList } from "../recoil/selectors/selector"
 import { useRecoilValue } from "recoil"
 
 export default function SelectBox() {
 
-    // 첫번째 select box
     const selectData = useRecoilValue(selectList);
 
     // 두번째 select box
     const [selectedType, setSelectedType] = useState(selectData.type.default);
+    const [selectedTaste, setSelectedTaste] = useState(selectData.taste[selectData.type.options[selectData.type.default].default].default);
+
+    useEffect(() => {
+        setSelectedTaste(selectData.taste[selectData.type.options[selectedType].default].default);
+    }, [selectedType]);
 
     console.log(selectData.type);
     console.log(selectData.type.default);
 
     console.log(selectData.type.options.food);
+
+    console.log(selectData.taste[selectData.type.options[selectData.type.default].default].default);
+
+
     return (
         <div className="w-96 h-96 m-auto mt-32 flex flex-col rounded-lg items-center bg-cyan-50">
             <div className="w-52 m-auto">
@@ -24,7 +32,7 @@ export default function SelectBox() {
                     id="type"
                     name="type"
                     className="mt-5 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue= {selectData.type.default}
+                    defaultValue={selectData.type.default}
                     onChange={e => setSelectedType(e.target.value)}
                 >
                     {
@@ -32,15 +40,17 @@ export default function SelectBox() {
                             <option value={typeName} key={typeName}>
                                 {typeName}
                             </option>
-                        ))    
+                        ))
                     }
                 </select>
                 <select
                     id="type"
                     name="type"
                     className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    value={selectData.type.options[selectedType].default}
-                    >
+                    value={selectedTaste}
+                    defaultValue={selectData}
+                    onChange={e => setSelectedTaste(e.target.value)}
+                >
                     {
                         selectData.type.options[selectedType].options.map((optionName) => (
                             <option value={optionName} key={optionName}>
@@ -50,14 +60,19 @@ export default function SelectBox() {
                     }
                 </select>
                 <select
-                    id="location"
-                    name="location"
+                    id="taste"
+                    name="taste"
                     className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    defaultValue="Canada"
+                    value={selectedTaste}
+                    onChange={e => setSelectedTaste(e.target.value)}
                 >
-                    <option>United States</option>
-                    <option>Canada</option>
-                    <option>Mexico</option>
+                    {
+                        selectData.taste[selectData.type.options[selectedType].default].options.map((optionName) => (
+                            <option value={optionName} key={optionName}>
+                                {optionName}
+                            </option>
+                        ))
+                    }
                 </select>
             </div>
         </div>
